7ab018281f35b4bc95930c01c8287405cbb7694a
Anant Sharma <anant98@hotmail.com>
2018-07-03 17:25:03 -0400

    I've got the javascript running for the tablet-mode sidebar.
    I've noticed some issues when resizing the window between the different widths I'm trying to account for. My idea so far is that I can fix these issues by restructuring the CSS so that the website is mobile-first.
    
    To be honest, that doesn't seem like it should take too long, but revisiting code I thought I was done with is a pain.
    
    Overall:
    
    - Implement mobile screen sizes.
    
    - Restructure to make mobile-first
    
    - The javascript issue of toggling the sidebar and then changing the screen width could be counteracted by using vw as the units. Try it out and see.

--- scripts.js
+++ scripts.js
@@ -3,22 +3,42 @@ var sbItems = document.getElementById('sidebar-items');
 var content = document.getElementById('content');
 var container = document.getElementById('container');
 
+
+// desktop = 0, tablet = 1, mobile = 2
+const sbPercentages = [["7%", "2%"], ["12%","5%"], [undefined, undefined]];
+
 var sbTog = false;
 
 function sideBarToggle()
 {
-    if(!sbTog)
+    var visualWidth = document.documentElement.clientWidth;
+    var deviceType;
+
+    if(visualWidth > 768)
     {
-        sb.style.width = "7%";
-        sbItems.style.visibility = "visible";
-        sbItems.style.opacity = 1;
-        sbTog = true;
+        deviceType = 0;
+    }
+    else if (visualWidth > 400)
+    {
+        deviceType = 1;
     }
     else
     {
-        sb.style.width = "2%";
-        sbItems.style.visibility = "hidden";
-        sbItems.style.opacity = 0;
-        sbTog = false;
+        deviceType = 2;
+    }
+
+    if(!sbTog)
+    {
+            sb.style.width = sbPercentages[deviceType][0];
+            sbItems.style.visibility = "visible";
+            sbItems.style.opacity = 1;
+            sbTog = true;
+    }
+    else
+    {
+            sb.style.width = sbPercentages[deviceType][1];
+            sbItems.style.visibility = "hidden";
+            sbItems.style.opacity = 0;
+            sbTog = false;
     }
 }
