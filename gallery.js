c98f6bbd9d36409ef2242d913dbe3b274c62349f
Anant Sharma <anant98@hotmail.com>
2018-07-07 21:36:18 -0400

    Implemented a modal setup for mobile screens.
    Need to start upscaling for tablets and desktop users now.
    
    Need to fix the navigation bar so that it's sticky to the top of the visual area.

--- gallery.js
+++ gallery.js
@@ -1,7 +1,10 @@
 var navBar = document.getElementById('navBar');
 var navItems = document.getElementById('navItems');
-
 var navTg = false;
+
+var modal = document.getElementById('modal');
+var modalImage = document.getElementById('modalImage');
+var modalCaption = document.getElementById('modalCaption');
 
 function navToggle()
 {
@@ -15,4 +18,19 @@ function navToggle()
         navItems.style.display = "none";
         navTg = false;
     }
+}
+
+function imageModal(element)
+{
+    var image = document.getElementsByTagName('img');
+    image = image[element-1];
+    console.log(image.src);
+    
+    var caption = document.getElementsByTagName('span');
+    caption = caption[element-1];
+
+
+    modalImage.src = image.src;
+    modalCaption.innerHTML = caption.innerHTML;
+    modal.style.display = "block";
 }
